d886c8f6acae020f1f46c223b5e332b7f21548a5
ztgoto <ztmailgo@gmail.com>
2018-02-06 18:14:30 +0800

    test

---
+++ cloud-auth/src/main/java/io/github/ztgoto/cloud/auth/configure/WebSecurityConfigurer.java
@@ -0,0 +1,21 @@
+package io.github.ztgoto.cloud.auth.configure;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class WebSecurityConfigurer extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+		.authorizeRequests()
+			.anyRequest().authenticated()
+			.and()
+//		.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+//		.and()
+		.formLogin();
+	}
+
+}
